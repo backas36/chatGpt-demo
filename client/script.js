5882ed77294054bd13ca1c011faa3488e181ce6d
backas36 <backas36@gmail.com>
2023-01-08 15:04:42 +0800

    update url

--- client/script.js
+++ client/script.js
@@ -68,7 +68,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server
-  const response = await fetch("http://localhost:8080", {
+  const response = await fetch("https://chatgpt-demo-i81v.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
